a902aeb180e553d7e3a9b7f03f0b44c234a333d0
saveunhappy <925147954@qq.com>
2022-05-08 04:16:35 +0000

    Update at 2022-05-08T04:16:35.115509Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,13 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        int count = 0;
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (Pattern.matches("[A-Z]", Character.toString(aChar))) {
+                count += 1;
+            }
+        }
+        return count;
     }
 }
