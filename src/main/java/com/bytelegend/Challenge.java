9eed23f4582a7ca75e952c857580242a1d12669b
karolk75 <karolk410410@gmail.com>
2022-07-05 08:10:44 +0000

    Update at 2022-07-05T08:10:44.196596Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,13 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        char[] parts = s.split();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (parts[i].isUpperCase()) {
+                count++;
+            }
+        }
+        return count;
     }
 }
