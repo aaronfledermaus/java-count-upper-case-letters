dbaecd2098bb84238582c260b7d8a8607222f270
chushi0 <wangyongchao49@sina.com>
2022-07-06 16:39:56 +0000

    Update at 2022-07-06T16:39:56.265268Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,12 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        int c = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                c++;
+            }
+        }
+        return c;
     }
 }
