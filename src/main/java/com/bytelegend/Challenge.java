0cb2e3993dee02bf5d862e247a739b1def84506e
chesterbachelor <kawalerbartosz@gmail.com>
2022-07-13 10:51:32 +0000

    Update at 2022-07-13T10:51:32.197279Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,12 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z')
+                count++;
+        }
+        return count;
     }
 }
