31dfc50ca3c501557180647b970458017f0a1187
BestMiltank <topjj0204@163.com>
2022-07-18 08:17:09 +0000

    Update at 2022-07-18T08:17:09.741767Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,12 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        int n = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s.toLowerCase().charAt(i)) {
+                n++;
+            }
+        }
+        return n;
     }
 }
