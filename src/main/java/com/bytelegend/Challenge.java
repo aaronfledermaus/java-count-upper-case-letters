857e5c6c51e16efd846578f0359831bbe92acd48
amine01laaboudi <al2000229@gmail.com>
2022-07-20 17:23:13 +0000

    Update at 2022-07-20T17:23:13.570790Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,12 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        int answer = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] <= 'Z' && s[i] >= 'A') {
+                ++answer;
+            }
+        }
+        return answer;
     }
 }
