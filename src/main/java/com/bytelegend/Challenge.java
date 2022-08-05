44ad085edb6e74b13286ea933070f3e395542c05
TaoYang1998 <1121758475@qq.com>
2022-08-05 13:57:33 +0000

    Update at 2022-08-05T13:57:33.270775Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,14 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                num++;
+            }
+        }
+
+        return num;
     }
 }
