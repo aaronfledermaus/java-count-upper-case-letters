52970f05ca11873949573933b09d48a303ce5437
ZusmanFate <demonsLwg@foxmail.com>
2022-09-07 09:30:28 +0000

    Update at 2022-09-07T09:30:28.826791Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,12 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                res++;
+            }
+        }
+        return res;
     }
 }
